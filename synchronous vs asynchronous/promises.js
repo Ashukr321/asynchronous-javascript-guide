ad777c9cf1b71c6aa8bbdb675b792c835af662ac
Ashutosh kumar <115258392+Ashukr321@users.noreply.github.com>
2026-01-19 17:26:47 +0000

    feat:promises static and instance methods

---
+++ synchronous vs asynchronous/promises.js
@@ -0,0 +1,33 @@
+// promises
+// Promises is the build  classes and constructor through which we can create the 
+// promises that take 2 paramertes 
+// parameterisse constructor 
+
+// static methods methods meeas we can call it directly without creating the object 
+
+// 1.Promises.all();
+
+//2 type of the methods 
+// static methods and instance methods 
+
+// static methods we can call it directly by using className 
+// example  
+// promises have the static methods and instance methods 
+
+// Promises
+
+Promises.all();
+Promises.allSettled();
+Promises.any();
+Promises.race();
+Promises.reject();
+Promises.resolve();
+Promises.try();
+
+// 2. instance methods of the javascript 
+// 1. then() : is the instance methods 
+// 2. catch(): 
+// 3. finally(): method
+
+
+
